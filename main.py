f230ab372dd6eeb8794c9efcfe8da53d0e8d42a9
James Hardjadinata <James Hardjadinata>
2017-02-26 23:27:16 -0800

    Add problem P17.7

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# The below DP problems return the DP arrays/matrices filled for potential further toying and analysis.
+
 # Page 295, The Pretty Printing Problem.
 # Given a set of words representing a sentence/excerpt, return the minimum messiness out of all the
 # configurations possible that the excerpt can be split into as lines.
 # The messiness is defined as the sum of (blanks in a given line) ** 2 over all lines.
-def f(words, L)-> int:
+def pretty_printing(words, L) -> "array of int":
     # Default initialize the DP list.
     min_messiness = [-1] * len(words)
     min_messiness[0] = (L - len(words[0])) ** 2
@@ -29,6 +31,28 @@ def f(words, L)-> int:
 
     return min_messiness
 
-s = ['aaa', 'bbb', 'c', 'd', 'ee', 'ff', 'ggggggg']
-length = 11
-print(f(s, length))
+# Page 288, The BedBathAndBeyond.com problem.
+# Given a phrase and a dictionary of words, determine whether a decomposition of the phrase into a subset of
+# the dictionary exists; if it does, return 1 such decomposition.
+def bbb(phrase, dict) -> "array of int":
+    last_word_length = [-1] * len(phrase)
+    for i in range(0, len(phrase)):
+        # If this substring is an actual string in the dict, set it as a valid decomposition.
+        if dict.count(phrase[0:i+1]):
+            last_word_length[i] = i + 1
+
+        # Otherwise, this substring is a decomposition of a smaller decomposition + dict word or not at all.
+        if last_word_length[i] == -1:
+            for j in range(0, i):
+                if last_word_length[j] != -1 and dict.count(phrase[j+1:i+1]):
+                    last_word_length[i] = i - j
+                    break
+
+    return last_word_length
+
+pp_s = ['aaa', 'bbb', 'c', 'd', 'ee', 'ff', 'ggggggg']
+bbb_d = ['a', 'am', 'an', 'man', 'plan', 'canal']
+bbb_str = "amanaplanacanal"
+pp_length = 11
+print(pretty_printing(pp_s, pp_length))
+print(bbb(bbb_str, bbb_d))
