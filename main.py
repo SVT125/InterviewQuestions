28b051cba677184753c50ec992d39b7e4ac6892b
James Hardjadinata <James Hardjadinata>
2017-03-23 00:23:03 -0700

    Rename variables and functions for PEP 8, clarity

--- main.py
+++ main.py
@@ -2,36 +2,36 @@ from functools import reduce
 
 
 # 0-1 Knapsack Problem w/ the famous O(nW) time but using only O(W) space; n = # of items, W = max weight.
-# A is a list of tuples (weight, value), W is a non-negative integer representing max weight.
+# items is a list of tuples (weight, value), max_weight is a non-negative integer representing max weight.
 # We let the partial array be size W+1, since we index the items with 1-indexing -> 0th index is the case.
-# Returns the max value attainable of A given W.
-def knapsack(A, W) -> int:
-    partial_results = [0] * (W+1)
-    for i in range(0, len(A)):
-        for j in range(W, 0, -1):
+# Returns the max value attainable of items given max_weight.
+def knapsack(items, max_weight) -> int:
+    partial_results = [0] * (max_weight+1)
+    for i in range(0, len(items)):
+        for j in range(max_weight, 0, -1):
             # Update right to left, since we don't want to use new values from earlier in the array for the later part.
             # If we can add the ith item + its better to take its value, add it on.
             # In all other cases given this 1D array implementation, do nothing as we already have the best value.
-            if A[i][0] <= j and partial_results[j - A[i][0]] + A[i][1] > partial_results[j]:
-                partial_results[j] = partial_results[j - A[i][0]] + A[i][1]
-    return partial_results[W]
+            if items[i][0] <= j and partial_results[j - items[i][0]] + items[i][1] > partial_results[j]:
+                partial_results[j] = partial_results[j - items[i][0]] + items[i][1]
+    return partial_results[max_weight]
 
 
-# Given an array of int A, return B such that
-# B[i] = A[j], j is the earliest value where j > i and A[i] >= A[j].
-# e.g. A = [5, 4, 6] -> B[0] = A[1] = 4.
-# A DP-like solution that identifies B[i] = B[i+1] | A[i+1] | 0.
+# Given an array of int nums, return B such that
+# B[i] = nums[j], j is the earliest value where j > i and nums[i] >= nums[j].
+# e.g. nums = [5, 4, 6] -> B[0] = nums[1] = 4.
+# nums DP-like solution that identifies B[i] = B[i+1] | A[i+1] | 0.
 # Takes O(n) time (and O(n) space to simply create the array returned).
-def earliest_lesser(A) -> "array of int":
-    B = [0] * len(A)
-    for i in range (len(A)-1, -1, -1):
-        if i == len(A)-1 or (i == len(A)-2 and A[i] < A[i+1]):
+def earliest_lesser(nums) -> "array of int":
+    B = [0] * len(nums)
+    for i in range (len(nums)-1, -1, -1):
+        if i == len(nums)-1 or (i == len(nums)-2 and nums[i] < nums[i+1]):
             # If we're filling the last element or the last element is greater than the second to last.
             B[i] = 0
-        elif A[i] > A[i+1]:
-            # If the next element in A is less than the current, it's the best answer.
-            B[i] = A[i+1]
-        elif A[i] > B[i+1]:
+        elif nums[i] > nums[i+1]:
+            # If the next element in nums is less than the current, it's the best answer.
+            B[i] = nums[i+1]
+        elif nums[i] > B[i+1]:
             # Otherwise, if the best element comes from past i+1.
             B[i] = B[i+1]
         else:
@@ -45,9 +45,9 @@ def earliest_lesser(A) -> "array of int":
 # That is, if the array is perfect it is exactly [0, n-1] w/ no duplicates.
 # If it isn't, the duplicated element takes the slot of what is now the missing element.
 # Returns a tuple of int (duplicate, missing).
-def find_dupe_and_missing(A) -> "tuple of int":
-    B = [i for i in range(0, len(A))]
-    C = A + B
+def find_dupe_and_missing(nums) -> "tuple of int":
+    B = [i for i in range(0, len(nums))]
+    C = nums + B
     xored_answers = reduce(lambda x, y: x ^ y, C)
     differing_bit = xored_answers & ~(xored_answers - 1)
 
@@ -59,7 +59,7 @@ def find_dupe_and_missing(A) -> "tuple of int":
     duplicate = -1
 
     # Traverse the whole array to find our unknown answer - if we've found it, we know it's not the missing element.
-    for x in A:
+    for x in nums:
         if x == unknown_answer:
             duplicate = unknown_answer
             break
@@ -73,7 +73,7 @@ def find_dupe_and_missing(A) -> "tuple of int":
 # Given a set of words representing a sentence/excerpt, return the minimum messiness out of all the
 # configurations possible that the excerpt can be split into as lines.
 # The messiness is defined as the sum of (blanks in a given line) ** 2 over all lines.
-def pretty_printing(words, L, return_array = True) -> "array of int":
+def pretty_printing(words, L, return_array=True) -> "array of int":
     # Default initialize the DP list.
     # line_separators[i] = the last line has words[j, i].
     min_messiness = [-1] * len(words)
@@ -109,11 +109,11 @@ def pretty_printing(words, L, return_array = True) -> "array of int":
 # EPI Page 288, The BedBathAndBeyond.com problem.
 # Given a phrase and a dictionary of words, determine whether a decomposition of the phrase into a subset of
 # the dictionary exists; if it does, return 1 such decomposition.
-def bbb(phrase, dict) -> "array of int":
+def bed_bath_and_beyond(phrase, dictionary) -> "array of int":
     last_word_length = [-1] * len(phrase)
     for i in range(0, len(phrase)):
         # If this substring is an actual string in the dict, set it as a valid decomposition.
-        if dict.count(phrase[0:i+1]):
+        if dictionary.count(phrase[0:i+1]):
             last_word_length[i] = i + 1
 
         # Otherwise, this substring is a decomposition of a smaller decomposition + dict word or not at all.
@@ -125,16 +125,16 @@ def bbb(phrase, dict) -> "array of int":
 
     return last_word_length
 
-ks_items = [(2, 3), (3, 4), (4, 5), (5, 6)]
-el = [5, 1, 3, 4, 6, 2]
-mm_array = [0, 1, 3, 2, 6, 4, 7, 1]
-pp_s = ['aaa', 'bbb', 'c', 'd', 'ee', 'ff', 'ggggggg']
-bbb_d = ['a', 'am', 'an', 'man', 'plan', 'canal']
-bbb_str = "amanaplanacanal"
-pp_length = 11
+knapsack_items = [(2, 3), (3, 4), (4, 5), (5, 6)]
+earlier_lesser_numbers = [5, 1, 3, 4, 6, 2]
+altered_numbers = [0, 1, 3, 2, 6, 4, 7, 1]
+paragraph = ['aaa', 'bbb', 'c', 'd', 'ee', 'ff', 'ggggggg']
+line_length = 11
+decomp_dict = ['a', 'am', 'an', 'man', 'plan', 'canal']
+decomp_str = "amanaplanacanal"
 
-print(knapsack(ks_items, 5))
-print(earliest_lesser(el))
-print(find_dupe_and_missing(mm_array))
-print(pretty_printing(pp_s, pp_length))
-print(bbb(bbb_str, bbb_d))
+print(knapsack(knapsack_items, 5))
+print(earliest_lesser(earlier_lesser_numbers))
+print(find_dupe_and_missing(altered_numbers))
+print(pretty_printing(paragraph, line_length))
+print(bed_bath_and_beyond(decomp_str, decomp_dict))
